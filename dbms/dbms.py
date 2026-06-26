8f208c0ff2f8c4f2f3ffbe6984174a6572a74e28
Nicolas Schmitt <nicolas@cedardb.com>
2026-06-26 12:05:21 +0200

    fix: make the YAML param parse units too (one line), then use the string form the schema wants

--- dbms/dbms.py
+++ dbms/dbms.py
@@ -122,7 +122,7 @@ class DBMS(ABC):
         self._numa_node = params["numa_node"] if "numa_node" in params else None
         self._cpuset_cpus = numa.get_cpus(self._numa_node)
         self._cpuset_mems = numa.get_mems(self._numa_node)
-        self._buffer_size = params["buffer_size"] if "buffer_size" in params and params["buffer_size"] is not None else numa.get_memory_size(self._numa_node) / 2
+        self._buffer_size = _parse_bytes(params["buffer_size"]) if "buffer_size" in params and params["buffer_size"] is not None else numa.get_memory_size(self._numa_node) / 2
         self._worker_threads = params["worker_threads"] if "worker_threads" in params and params["worker_threads"] is not None else numa.get_thread_count(self._numa_node)
         self._index = DBMS.Index.from_string(params.get("index", "primary"))
         self._version = params.get("version", "latest")
